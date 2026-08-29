63a1851fdb9b98dc5351fa05347cf39088cd7d00
Aarit Jayaswal <aaritrj@gmail.com>
2026-08-28 18:37:11 -0700

    uyjtjgh

--- src/_03_typing_tutor/Typer.java
+++ src/_03_typing_tutor/Typer.java
@@ -1,22 +1,46 @@
 package _03_typing_tutor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.*;
 
-public class Typer {
+public class Typer implements KeyListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	char currentLetter;
 
 	void setup(){
+		frame.addKeyListener(this);
 		currentLetter = generateRandomLetter();
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		label.setFont(label.getFont().deriveFont(500.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setText("" + currentLetter);
+		panel.add(label);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	char generateRandomLetter() {
 	    Random r = new Random();
 	    return (char) (r.nextInt(26) + 'a');
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		currentLetter = generateRandomLetter();
+		label.setText("" + currentLetter);
 	}
 }
--- src/_03_typing_tutor/TyperRunner.java
+++ src/_03_typing_tutor/TyperRunner.java
@@ -5,7 +5,8 @@ import java.util.*;
 public class TyperRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Typer typer = new Typer();
+		typer.setup(); 
 
 	}
 	
